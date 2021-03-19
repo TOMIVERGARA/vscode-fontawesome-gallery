1bd56a336626ee4abd2a7e438389b4e9b73d110d
Tomas Vergara <tominacho@hotmail.com>
2021-03-19 16:33:35 -0300

    Added new tool

--- fontawesometools.js
+++ fontawesometools.js
@@ -33,6 +33,40 @@ function createSearchIndexArrayFromIconsObj(iconsObjPath){
     }
 }
 
+function createFormattedIconsFile(iconsObjPath){
+    try {
+      if (fs.existsSync(iconsObjPath)) {
+        let rawData = fs.readFileSync(iconsObjPath);
+        let iconsList = JSON.parse(rawData)
+        let length = 0;
+        const IconsObject = {};
+        for(const icon in iconsList){
+            const entry = iconsList[icon];
+            const formattedName = `fag_${icon.replace('-', '_')}`;
+            const formattedIconEntry = {
+                changes: entry.changes,
+                name: icon,
+                label: entry.label,
+                search: entry.search,
+                styles: entry.styles,
+                unicode: entry.unicode,
+                voted: entry.voted
+            }
+            console.log(`${icon} => \u001b[1;32mReady! \u001b[0;39m`);
+            IconsObject[formattedName] = formattedIconEntry;
+            length++;
+        }
+        console.log(`\u001b[1;32mParsed and formatted ${length} icon entries successfuly. \u001b[0;39m`);
+        let output = JSON.stringify(IconsObject);
+        return fs.writeFileSync('fagIcons.json', output);
+      }else{
+        return console.error('Could not locate the icons.json file in the specified path: ' + iconsObjPath);
+      }
+    } catch(err) {
+      return console.error('There was an error while saving the file.\n➥ ' + err);
+    }
+}
+
 function printHelp(){
     console.log('\u001b[1;39mFontAwesome Tools is a CLI specifically designed to set up and update the VSCode Font Awesome Gallery extension. It can parse new icons.json files and much more. \u001b[0;39m \n');
     console.log('Usage syntax:');
@@ -50,7 +84,13 @@ function printHelp(){
               if(!argv.path) return console.log('You must specify an icons.json path using the "--path=" flag. \n\u001b[1;31mEarly exiting... \u001b[0;39m');
               console.log('Creating search index from icons object.');
               console.log('➥ icons.json path: ' + argv.path)
-              createSearchIndexArrayFromIconsObj(argv.path);
+              setTimeout(() => createSearchIndexArrayFromIconsObj(argv.path), 2000);
+              break;
+          case 'cfi':
+              if(!argv.path) return console.log('You must specify an icons.json path using the "--path=" flag. \n\u001b[1;31mEarly exiting... \u001b[0;39m');
+              console.log('Creating formatted icons file from icons object.');
+              console.log('➥ icons.json path: ' + argv.path)
+              setTimeout(() => createFormattedIconsFile(argv.path), 2000)
               break;
           default:
               printHelp();
